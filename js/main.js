4ac4a707e0116df350f0dd8774e5b4bd6e15d905
Leav Chan Dara <73814686+Chandara-97@users.noreply.github.com>
2020-12-21 22:03:11 +0700

    Web Design HTML CSS JS

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// **********************************************************************************************contact us********************************************************************************************
+const GTInput=document.querySelectorAll(".gt-input");
+
+for (let i=0; i< GTInput.length ; i++){
+   
+    let currentlabel = GTInput[i].parentElement.firstElementChild;
+
+    GTInput[i].addEventListener("focus",function(){
+        currentlabel.classList.add("move-up");
+        })
+
+        GTInput[i].addEventListener("blur",function(){
+            if( GTInput[i].value == ""){
+            currentlabel.classList.remove("move-up");
+            }
+            })
+
+
+}
+// **********************************************************************************************contact us********************************************************************************************
